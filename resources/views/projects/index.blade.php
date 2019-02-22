4a3e90c1689ac0d49c0f59aec9c0936f30f248d5
Nero <reymark.torres08.dev@gmail.com>
2019-02-22 11:33:46 +0800

    Remove bootstrap & scaffold Tailwind CSS

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,18 +2,21 @@
 
 @section('content')
     <div class="flex items-center mb-3">
+        <h1 class="mr-auto">My Projects</h1>
         <a href="/projects/create">New Project</a>
     </div>
 
-    <ul>
+    <div class="flex">
         @forelse ($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}">
-                    {{ $project->title }}
-                </a>
-            </li>
+            <div class="bg-white mr-4 rounded p-5 shadow w-1/3" style="height: 200px; ">
+                <h3 class="font-normal text-xl py-4 mb-4">{{ $project->title }}</h3>
+
+                <div class="text-grey">
+                    {{ str_limit($project->description, 100) }}
+                </div>
+            </div>
         @empty
-            <li>No projects yet.</li>
+            <div>No projects yet.</div>
         @endforelse
-    </ul>
+    </div>
 @endsection
