3b14e01bc9c32f867c6137159bcb1f02529c1068
Reymark Torres <reymark.torres08.dev@gmail.com>
2019-10-26 19:11:29 +0800

    Ep 23: Inviting Users As A Feature Test

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Facades\Tests\Setup\ProjectFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Facades\Tests\Setup\ProjectFactory;
 use Tests\TestCase;
 
 class InvitationsTest extends TestCase
@@ -12,7 +13,49 @@ class InvitationsTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    function a_project_can_invite_a_user()
+    public function non_owners_may_not_invite_users()
+    {
+        $project = ProjectFactory::create();
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)
+            ->post($project->path() . '/invitations')
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    function a_project_owner_can_invite_a_user()
+    {
+        $this->withoutExceptionHandling();
+
+        // Given I have a project
+        $project = ProjectFactory::create();
+
+        $userToInvite = factory(User::class)->create();
+
+        $this->actingAs($project->owner)->post($project->path() . '/invitations', [
+            'email' => $userToInvite->email
+        ])->assertRedirect($project->path());
+
+        $this->assertTrue($project->members->contains($userToInvite));
+    }
+
+    /** @test */
+    public function the_email_address_must_be_associated_with_a_valid_birdboard_account()
+    {
+        $project = ProjectFactory::create();
+
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+                'email' => 'notauser@example.com'
+            ])
+            ->assertSessionHasErrors([
+                'email' => 'The user you are inviting must have a Birdboard account.'
+            ]);
+    }
+
+    /** @test */
+    function invited_users_may_update_project_details()
     {
         // Given I have a project
         $project = ProjectFactory::create();
